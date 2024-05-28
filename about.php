f1a94fb7c2327c743f75594c03d7ee9d381ca476
Wahri Harma <wahrychaniago@gmail.com>
2024-05-28 23:09:19 +0700

    gasss

---
+++ about.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="app.css">
+
+  <title>Wahri Harma</title>
+</head>
+
+<body>
+
+  <!-- Intro -->
+
+  <div class="container-fluid">
+
+    <!-- Navbar -->
+    <?php require "navbar.html" ?>
+    <!-- Batas Navbar -->
+
+    <section class="container container-fluid">
+      <div class="row">
+
+        <div class="col-lg-6 position-relative">
+          <div class="col col col-md-12 position-relative top-50 start-50 translate-middle">
+            <div class="row text-white">
+              <h3> About</h3>
+              <p class="h6">wahrychaniago98@gmail.com</p>
+              <br>
+              <p>
+                I am a web developer. My technical skills include the programming languages HTML, CSS, JavaScript, and
+                PHP, as well as the use of frameworks such as React.js and Laravel. I also have expertise in MySQL and
+                MongoDB databases.
+              </p>
+              <br>
+              <p>Kabupaten Lima Puluh Kota</p>
+              <p>Sumatera Barat, indonesia</p>
+              <div class="d-grid gap-2 d-md-block">
+                <button type="button" class="btn btn-outline-light">Services</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="m-3">
+            <img src="Images/Avatar.jpeg" class="img-fluid rounded" alt="">
+          </div>
+        </div>
+
+      </div>
+
+    </section>
+  </div>
+
+  <!-- Batas Intro -->
+
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+    crossorigin="anonymous"></script>
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="app.css">
+
+  <title>Wahri Harma</title>
+
+</head>
+
+<body>
+
+  <!-- Intro -->
+  <div class="container-fluid">
+
+    <!-- Navbar -->
+    <?php require "navbar.html" ?>
+    <!-- Batas Navbar -->
+
+    <section class="container-fluid">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="p-3">
+            <img src="" class="img-fluid" alt="">
+          </div>
+        </div>
+
+        <div class="col-lg-6 position-relative">
+          <div class="col col-md-12 position-relative top-50 start-50 translate-middle">
+            <div class="row">
+              <h3 class="text-white"> Hello...</h2>
+                <p class="h6 text-white">I am a professional</p>
+                <h1 class="text-white">WEB DEVELOPER</h1>
+                <p class="text-white">
+                  I am a web developer. My technical skills include the programming languages HTML, CSS, JavaScript, and
+                  PHP, as well as the use of frameworks such as React.js and Laravel. I also have expertise in MySQL and
+                  MongoDB databases.
+                </p>
+            </div>
+            <div class="text-align-right">
+              <button type="button" class="btn btn-outline-light">Resume</button>
+              <button type="button" class="btn btn-outline-light">Portfolio</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+  </div>
+
+  <!-- Batas Intro -->
+
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+    crossorigin="anonymous"></script>
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
